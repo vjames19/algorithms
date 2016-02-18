9bb3eadba4b72d17f824f07805f5d102349ea9bc
Victor J. Reventos <victor.reventos1@gmail.com>
2016-02-17 23:07:35 -0600

    Update linked list style

--- algorithm/src/main/kotlin/com/github/vjames19/datastructures/linkedlist/LinkedList.kt
+++ algorithm/src/main/kotlin/com/github/vjames19/datastructures/linkedlist/LinkedList.kt
@@ -1,23 +1,24 @@
-
 package com.github.vjames19.datastructures.linkedlist
 
 /**
  * Created by vjames19 on 2/17/16.
  */
 interface LinkedList<E> {
-    fun size() : Int
+    fun size(): Int
 
-    fun isEmpty() : Boolean
+    fun isEmpty(): Boolean
 
-    fun first() : E?
+    fun first(): E?
 
-    fun last() : E?
+    fun last(): E?
 
-    fun addFirst(element : E)
+    fun addFirst(element: E)
 
-    fun addLast(element : E)
+    fun addLast(element: E)
 
-    fun removeFirst() : E?
+    fun removeFirst(): E?
+
+    fun removeLast(): E?
 
     fun clear()
 }
