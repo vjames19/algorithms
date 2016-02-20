b533968c4a067c0af72b082000b36eaa8e7d6fef
Victor J. Reventos <victor.reventos1@gmail.com>
2016-02-20 12:00:34 -0600

    Binary Search Tree

--- algorithm/src/main/kotlin/com/github/vjames19/datastructures/tree/BinarySearchTree.kt
+++ algorithm/src/main/kotlin/com/github/vjames19/datastructures/tree/BinarySearchTree.kt
@@ -32,7 +32,7 @@ class BinarySearchTree<Key: Comparable<Key>, Value> {
         }
     }
 
-    private fun put(key: Key, value: Value) {
+    fun put(key: Key, value: Value) {
         root = put(root, key, value)
     }
 
@@ -54,4 +54,82 @@ class BinarySearchTree<Key: Comparable<Key>, Value> {
             return root
         }
     }
+
+    fun remove(key: Key) {
+        root = remove(root, key)
+    }
+
+    private fun remove(root: Node<Key, Value>?, key: Key): Node<Key, Value>? {
+        if (root == null) {
+            return null
+        } else {
+            val comparison = key.compareTo(root.key)
+
+            if (comparison < 0) {
+                root.left = remove(root.left, key)
+            } else if (comparison > 0) {
+                root.right = remove(root.right, key)
+            } else {
+                size--
+
+                if (root.right == null) return root.left
+                if (root.left == null) return root.right
+
+                val temp = root
+                val replacement = min(temp.right)!!
+                replacement.left = root.left
+                replacement.right = remove(temp.right, replacement.key)
+                return replacement
+            }
+
+            return root
+        }
+    }
+
+    fun min(): Key? {
+        return min(root)?.key
+    }
+
+    private tailrec fun min(root: Node<Key, Value>?): Node<Key, Value>? {
+        if (root?.left == null) {
+            return root
+        } else {
+            return min(root?.left)
+        }
+    }
+
+    fun max(): Key? {
+        return max(root)?.key
+    }
+
+    private tailrec fun max(root: Node<Key, Value>?): Node<Key, Value>? {
+        if (root?.right == null) {
+            return root
+        } else {
+            return max(root?.right)
+        }
+    }
+
+    fun floor(key: Key): Key? {
+        return floor(root, key)?.key
+    }
+
+    private fun floor(root: Node<Key, Value>?, key: Key): Node<Key, Value>? {
+        if (root == null) {
+            return null
+        } else {
+            val comparison = key.compareTo(root.key)
+
+            if (comparison == 0) {
+                return root
+            } else if (comparison < 0) {
+                return floor(root.left, key)
+            } else {
+                val node = floor(root.right, key)
+                return node ?: root
+            }
+        }
+    }
+
+
 }
